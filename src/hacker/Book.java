f8592519bc65fec42d59e9609e91ed4f0c53787d
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2019-09-19 11:16:56 -0700

    add getter

--- src/hacker/Book.java
+++ src/hacker/Book.java
@@ -4,4 +4,16 @@ public class Book {
     String name;
     String author;
     String isbn;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
