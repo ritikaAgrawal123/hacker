3ffbdda4d2b64ac8d69df1a4969ad685cd6491f5
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2019-09-19 11:33:24 -0700

    add input output statements

--- src/hacker/Solution.java
+++ src/hacker/Solution.java
@@ -6,6 +6,17 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        scanner.close();
-    }
+        Book[] book = new Book[n];
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            scanner.nextLine();
+            for (int i = 0; i < n; i++) {
+                String name = scanner.nextLine();
+                String author = scanner.nextLine();
+                String isbn = scanner.nextLine();
+                book[i] = new Book(name, author, isbn);
+                System.out.println(book[i]);
+                scanner.close();
+            }
 }
