20b805e30e6887b73ac8c470f08996574b6f8497
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2019-09-19 11:29:47 -0700

    add scanner class

--- src/hacker/Solution.java
+++ src/hacker/Solution.java
@@ -1,4 +1,11 @@
 package hacker;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+    }
 }
